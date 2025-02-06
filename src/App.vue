182d9505edd046cf7e7384def3872edaf6158a83
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-02-06 14:48:07 -0300

    feat Calculator

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from "vue";
+
+const num1 = ref(0);
+const num2 = ref(0);
+const operacao = ref("+");
+
+const resultado = computed(() => {
+  switch (operacao.value) {
+    case "+":
+      return num1.value + num2.value;
+    case "-":
+      return num1.value - num2.value;
+    case "*":
+      return num1.value * num2.value;
+    case "/":
+      return num2.value !== 0
+        ? num1.value / num2.value
+        : "Impossível realizar divisão por 0";
+    default:
+      return 0;
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="calculator">
+    <h2>Minha Calculadora com VUE</h2>
+    <input type="number" v-model.number="num1" placeholder="Digite um número" />
+    <select v-model="operacao">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">×</option>
+      <option value="/">÷</option>
+    </select>
+    <input
+      type="number"
+      v-model.number="num2"
+      placeholder="Digite outro número"
+    />
+    <h3>Resultado: {{ resultado }}</h3>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+input,
+select {
+  margin: 10px;
+  padding: 5px;
+  font-size: 18px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
